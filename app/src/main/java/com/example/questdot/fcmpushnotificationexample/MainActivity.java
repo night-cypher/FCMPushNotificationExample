777aa0fdcb98b391931f018d5788f83cd2eceef7
HP <krist.ng@corous360.com>
2016-08-03 14:01:00 +0800

    update

--- app/src/main/java/com/example/questdot/fcmpushnotificationexample/MainActivity.java
+++ app/src/main/java/com/example/questdot/fcmpushnotificationexample/MainActivity.java
@@ -15,7 +15,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                String value = getIntent().getExtras().getString(key);
+                Log.d("main", "Key: " + key + " Value: " + value);
+            }
+        }
         Button subscribeButton = (Button) findViewById(R.id.subscribeButton);
         subscribeButton.setOnClickListener(this);
 
